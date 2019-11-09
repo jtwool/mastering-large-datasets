584cdef1c3fa6d5df777f9de189d7fc5bcbc2e84
JT W <jwolohan@gmail.com>
2019-11-09 13:42:03 -0500

    Update README, requirements, gitignore

--- Ch08/common-errors.py
+++ Ch08/common-errors.py
@@ -8,8 +8,9 @@ class ErrorCounter(MRJob):
 
   def reducer(self, key, vals):
     num_404s = sum(vals)
-    if num_404s>5:
+    if num_404s>0:
       yield key, num_404s
 
 if __name__ == "__main__":
-  ErrorCounts.run()
+  ErrorCounter.run()
+
